24a5f6e458dcf6ae40f55cf4e17d328acd2affd9
dmoseykin <dmoseykin@phoenixit.ru>
2022-06-23 16:43:02 +0300

    Добавлен плагин checkstyle и таск dockerDeploy

--- producer-service/src/main/java/com/example/producerservice/service/ProducerServiceImpl.java
+++ producer-service/src/main/java/com/example/producerservice/service/ProducerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.producerservice.service;
 
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
